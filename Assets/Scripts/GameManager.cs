68afcaf38279ff552fc9fad866ae593818ab111c
Unknown <dockit@gmail.com>
2019-03-15 19:19:23 +1100

    Basic game states (pause, playing, etc)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,17 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
+public enum GameState {
+    Playing,
+    Paused
+}
+
+public class GameManager : MonoBehaviour {
+    public GameState gameState = new GameState();
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        gameState = GameState.Playing;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            gameState = gameState == GameState.Paused ? GameState.Playing : GameState.Paused;
+            Debug.Log(gameState);
+        }
     }
 }
